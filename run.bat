ac43c0bcccfb26499b3bc5bf93ce9074807497dd
brumleger <cbrumleger@gmail.com>
2022-09-19 11:02:04 +0200

    Made some changes

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-"D:\git\bin\git.exe" config --global --unset credential.helper
+"D:\git\bin\git.exe" config --global credential.helper store
 cd D:\projects\voorbeeld-myfirstapp
 "D:\git\bin\git.exe" add --all
 "D:\git\bin\git.exe" commit -m "Made some changes"
