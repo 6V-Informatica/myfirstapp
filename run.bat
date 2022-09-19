e046904465b8d0b23bfc413a5bc993baa4692256
brumleger <cbrumleger@gmail.com>
2022-09-19 10:49:08 +0200

    Made some changes

---
+++ run.bat
@@ -0,0 +1,6 @@
+"D:\git\bin\git.exe" config --global user.name "brumleger"
+cd D:\projects\voorbeeld-myfirstapp
+"D:\git\bin\git.exe" add --all
+"D:\git\bin\git.exe" commit -m "Made some changes"
+"D:\git\bin\git.exe" push origin main
+pause
